ff7d942c0fabede2aa06d8b491a99f9426c3aad7
Luke NOBLE <noble@gevmac04.ens.fr>
2019-10-30 12:21:50 +0100

    MWT to wcon

--- multiwormtracker/blobs2wcon.R
+++ multiwormtracker/blobs2wcon.R
@@ -3,52 +3,41 @@
 #######################################################################
 # LMN Aug 2019
 # conversion of MWT blobs to WCON
+# https://github.com/openworm/tracker-commons/blob/master/WCON_format.md
 
-# length, width, 11-point skeleton, and outline retained
+# time, x/y, length/width (in @MWT slot) at minimum
+# by default, filtered to only tracks with data for (11-point) skeleton and outline
 # pixels converted to mm, assuming 0.026mm per pixel
-# outlines are unaltered from Rex's compressed pixel-based form and so are given as a length 4 string array:
-# x & y of starting pixel, number of pixels in outline, then ASCII string giving bit pairs giving offset coord/direction + magnitudes from starting pixel)
-# see https://github.com/openworm/tracker-commons
-# {
-#   "metadata":{
-#     "who":"Open Worm",
-#     "timestamp":"2016-01-22T17:44:48",
-#     "protocol":"Numbers made up by hand for an example!"
-#   },
-#   "units":{"t":"s", "x":"mm", "y":"mm", "length":"mm", "width":"mm", "outline":"rex"},
-#   "data":[
-#     {"id":"worm1", "t":[0.1], "x":[[0.33, 0.65, 0.8, 1.1, 1.2, ...]], "y":[[2.31, 2.25, 2.0, 1.87, 1.66, ...]], "length":[[40.1]], "width":[[3.1]], "outline":[["11", "911", "118", "MMMeeMUGEEUUFFIIUVIVPc<<c<3<30`02<0228P0"]]},
-#     {"id":"worm1", "t":[0.3], "x":[[0.27, 0.6, 0.75, 1.0, 1.1, ...]], "y":[[2.4, 2.3, 2.07, 1.78, 1.75, ...]]}, "length":[[64.3]], "width":[[13.2]], "outline":[["1040", "2303", "237", "MMGGMGEGEEeEMMEWedPX82080020202280P"]]}
-#     ]
-# }
+# outlines (walks) are unaltered from Rex's compressed pixel-based form:
+# walk = [{px=[x & y of starting pixel, pixel length=1], n=number of pixels in outline, '4'=base64 encoded binary array giving x/y sign from starting pixel}]
 
 # pass a config file (delimited, headered) listing per line:
 # (1) a directory containing blobs files,
-# (2) a prefix for the .wcon.zip (if blank/NA, the parent directory is used)
+# (2) a prefix for the .wcon.zip output (if blank/NA, the parent directory is used)
 # (3:N) metadata (with names taken from the header).
 # For CeMEE: 
-#   sample      RIL/population ID
-#   sample_type "RIL", "population"
+#   sample      unique ID
+#   sample_type RIL, NIL, population, founder, MA line, MA founder etc
+#   who         experimenter
+#   place       Paris/Lisbon
 #   date        YYYYMMDD
 #   time        HHMMSS
-#   who         experimenter
 #   media       NGM/NaCl
-#   temperature C
-#   humidity    %
-#######################################################################
+
+# output is written to the each blobs directory, then zipped along with a sha1 hash
+###################################################################################
 
 require(jsonlite)
 require(data.table)
 require(parallel)
-library(lubridate)
+require(lubridate)
+require(dplyr)
 
 args = commandArgs(trailingOnly = T)
 cfg  = args[1] # config file 
 
 # pixel > mm conversion
 mmperpixel = 0.026
-# json converter
-toj = jsonlite::toJSON
 # unarray unit values recursively
 unb <- function(i){
   if(length(i)==1){
@@ -68,33 +57,47 @@ unitl = lapply(
        y='mm',
        length='mm',
        width='mm',
-       outline='rex', 
-       temperature='C',
-       humidity='%',
        food='HT115',
-       media='NGM',
        software=list(name='MWT', version='1.3.0_r1035')), 
   function(i) unb(i)
 )
 
-parseblobs <- function(blobf){
+parseblobs <- function(blobf, filterOnOutline=T){
   
   blockparser <- function(b, wormid){
     
-    nl = length(b)
     cols = lapply(b, function(l) {l=strsplit(l, ' ')[[1]]; l[l!='']})
     xes = lapply(cols, function(l) as.numeric(l[3])*mmperpixel)
     yes = lapply(cols, function(l) as.numeric(l[4])*mmperpixel)
-    skeloffsets = lapply(cols, function(l) as.numeric(l[12:33])*mmperpixel)
     
-    list(id = unbox(wormid), 
-         t = sapply(cols, function(l) as.numeric(l[2])),
-         x = lapply(1:nl, function(i) xes[[i]] + skeloffsets[[i]][seq(1, 22, 2)]),
-         y = lapply(1:nl, function(i) yes[[i]] + skeloffsets[[i]][seq(2, 22, 2)]),
-         length = sapply(cols, function(l) as.numeric(l[9])*mmperpixel),
-         width = sapply(cols, function(l) as.numeric(l[10])*mmperpixel),
-         outline = lapply(cols, function(l) c(as.numeric(l[35:37]), l[[38]]))
-    )
+    if(filterOnOutline){
+    
+      skeloffsets = lapply(cols, function(l) as.numeric(l[12:33])*mmperpixel)
+      nl = length(b)
+      
+      list(id = unbox(wormid), 
+           t = sapply(cols, function(l) as.numeric(l[2])),
+           x = lapply(1:nl, function(i) xes[[i]] + skeloffsets[[i]][seq(1, 22, 2)]),
+           y = lapply(1:nl, function(i) yes[[i]] + skeloffsets[[i]][seq(2, 22, 2)]),
+           "@MWT" = list(length = sapply(cols, function(l) as.numeric(l[9])*mmperpixel),
+                         width = sapply(cols, function(l) as.numeric(l[10])*mmperpixel)
+           ),
+           walk = list(px = lapply(cols, function(l) c(as.numeric(l[35:36]),1)),
+                       n = sapply(cols, function(l) as.numeric(l[37])),
+                       "4" = sapply(cols, function(l) l[38])
+           )
+      )
+    } else {
+      # just x,y,length,width
+      list(id = unbox(wormid), 
+           t = sapply(cols, function(l) as.numeric(l[2])),
+           x = unlist(xes),
+           y = unlist(yes),
+           "@MWT" = list(length = sapply(cols, function(l) as.numeric(l[9])*mmperpixel),
+                         width = sapply(cols, function(l) as.numeric(l[10])*mmperpixel)
+           )
+      )
+    }
   }
   
   lines_ = readLines(blobf)
@@ -103,26 +106,31 @@ parseblobs <- function(blobf){
   
   parsed = lapply(seq_along(ix), function(i) {
     if(i==length(ix)){
-      datal = (ix[i]+1):length(lines_)
+      b = lines_[(ix[i]+1):length(lines_)]
     } else {
-      datal = (ix[i]+1):(ix[i+1]-1)
+      b = lines_[(ix[i]+1):(ix[i+1]-1)]
     }
-    tryCatch(blockparser(lines_[datal], wormids[i]), error = function(e){NULL})  
+    tryCatch(blockparser(b, wormids[i]), error = function(e){NULL})  
   })
   
-  # many lines do not have skeleton/outlines, ignoring them
-  # lls = unlist(lapply(lines_, function(i) length(strsplit(i, ' ')[[1]])))
-  # table(lls)
-  isnull = sapply(parsed, function(i) is.null(unlist(i)))
-  sprintf('%s incomplete data lines rmeoved')
-  data_ = parsed[!isnull]
+  if(filterOnOutline){
+    # many lines do not have skeleton/outlines, ignoring them
+    # lls = unlist(lapply(lines_, function(i) length(strsplit(i, ' ')[[1]])))
+    # table(lls)
+    isnull = sapply(parsed, function(i) is.null(unlist(i)))
+    cat(sprintf('\t%s/%s tracks without skeleton/outline removed\n', sum(isnull), length(isnull)))
+    data_ = parsed[!isnull]  
+  } else {
+    data_ = parsed
+  }
+  
   return(data_)
+  
 }
 
 main <- function(cfg, np=1){
   
-  cfg = '~/Documents/cemee/MWT2WCON/mwt_config'
-  config = fread(cfg, data.table = F)
+  config = fread(cfg, data.table = F, colClasses = c(date='character', time='character'))
   ndir = nrow(config)
   blobdir = config[,1]
   prefs = config[,2]
@@ -138,19 +146,83 @@ main <- function(cfg, np=1){
     blobsf = Sys.glob(sprintf('%s/*.blobs', diri))[1:4]
     pref = ifelse(is.na(prefs[i]), basename(diri), prefs[i])
     outf = sprintf('%s/%s.wcon', blobdir[i], pref)
+    osha = sprintf('%s.sha', outf)
+    ozip = sprintf('%s.zip', outf)
     
-    oi = list(
-      metadata = unb(c(metal, metadata[i,])),
-      units = unitl,
-      data = unlist(mclapply(blobsf, mc.cores=np, function(f) parseblobs(f)), recursive = F)
-    )
+    if(!file.exists(ozip)){
+      cat(sprintf('converting %s: %s blobs\n', diri, length(blobsf)))
+      
+      oi = list(
+        metadata = unb(c(metal, metadata[i,])),
+        units = unitl,
+        data = unlist(mclapply(blobsf, mc.cores=np, function(f) parseblobs(f, filterOnOutline = F)), recursive = F)
+      )
+      
+      write_json(oi, outf, pretty=T)
+      system(sprintf('shasum %s > %s', outf, osha))
+      zip(ozip, files = c(outf, osha))
+      system(sprintf('rm %s %s', outf, osha))  
+    } else {
+      cat(sprintf('skipping %s\n', diri))
+    }
     
-    write_json(oi, outf, pretty=T)
-    zip(paste0(outf, '.zip'), outf)
-    system(sprintf('rm %s', outf))
   })
   
 }
 
-main(cfg, np=4)
+# cfg = '~/Documents/cemee/MWT2WCON/mwt_config'
+cfg = '~/Documents/cemee/MWT2WCON/fm_blob.cfg'
+main(cfg, np=1)
+
+makeFMcfg <- function(){
+  
+  # make cfg (for the raw data that we have)
+  blobsParentDir = '/Volumes/GEV_SAV_01/mallard/behavioral_data/Raw_behavior/'
+  dirs = data.frame(dir = system(sprintf('find %s -type d -mindepth 2', blobsParentDir), intern=T), stringsAsFactors = F)
+  # dirs <- fread('~/Documents/cemee/MWT2WCON/fm_blob_dirs', header = F); names(dirs) = 'dir'
+  dirs = dirs[grep('^2', basename(dirs$dir)),,drop=F]
+  dirs$date = tstrsplit(basename(dirs$dir), '_')[[1]]
+  dirs$time = tstrsplit(basename(dirs$dir), '_')[[2]]
+  dirs$block = basename(dirname(dirs$dir))
+  (bt = table(dirs$block))
+  # names(bt)[names(bt) %in% metad$data_group_name]
+  # names(bt)[!names(bt) %in% metad$data_group_name]
+  
+  # meh ignore metadata. get line/food for blobs filenames
+  # metad <- fread('~/Documents/cemee/MWT2WCON/Luke_header.txt')
+  # names(metad)[6:7] = c('date', 'time')
+  
+  dirm = bind_rows(lapply(dirs$dir, function(i) {
+    print(i)
+    fs = basename(system(sprintf('ls %s/*blobs', i), intern = T, ignore.stderr = T))
+    if(len(fs)>0){
+      sp = tstrsplit(fs, '_')
+      id = unique(sp[[1]])
+      media = unique(sp[[2]])
+      block = substr(unique(sp[[3]]), 1, 4)
+      stopifnot(all(len(line)==1, len(food)==1, len(block)==1))
+      data.frame(dir = i, id, media, block, stringsAsFactors = F)  
+    }
+  }))
+  
+  mdir <- merge(dirs, dirm)
+  mdir$pref = ''
+  mdir$who = 'FM'
+  mdir$place = 'Paris'
+  mdir$sample = gsub('L0', '', mdir$id)
+  mdir$sample[mdir$id=='N2anc' & !mdir$block %in% c('B302','B305')] = 'N2'
+  mdir$sample_type = NA
+  mdir$sample_type[grep('^PB250', mdir$sample)] <- 'MA line'
+  mdir$sample_type[grep('^N2250', mdir$sample)] <- 'MA line'
+  mdir$sample_type[mdir$sample=='PBanc'] = 'MA founder'
+  mdir$sample_type[mdir$sample=='N2'] = 'MA founder'
+  ix = grep('L', mdir$sample, invert = T); ix = ix[is.na(mdir$sample_type[ix])]
+  mdir$sample_type[ix] = 'population'
+  mdir$sample_type[mdir$sample %in% cemeeFounders] <- 'founder'
+  mdir$sample_type[is.na(mdir$sample_type)] = 'RIL'
+  # exc. male depleted samples
+  mdir = mdir[grep('noM', mdir$sample, invert = T),]
+  mdir = mdir[,c('dir','pref','sample','sample_type','who', 'place','date','time','media')]
+  fwrite(mdir, '~/Documents/cemee/MWT2WCON/fm_blob.cfg')
+}
 
---
+++ multiwormtracker/rda2wcon.R
@@ -0,0 +1,226 @@
+#!/usr/bin/env Rscript
+
+#######################################################################
+# LMN Aug 2019
+# conversion of parsed Choreography output in R data container to WCON
+
+# time, x/y, angular(turningRate)/area/bias/curve(curvature)/length/speed(velocity)/width (where present, in @MWT slot)
+# see https://github.com/openworm/tracker-commons
+# {
+#   "metadata":{
+#     "who":"Open Worm",
+#     "timestamp":"2016-01-22T17:44:48",
+#     "protocol":"Numbers made up by hand for an example!"
+#   },
+#   "units":{"t":"s", "x":"mm", "y":"mm", "length":"mm", "width":"mm", "outline":"rex"},
+#   "data":[
+#     {"id":"worm1", "t":[0.1], "x":[[0.33, 0.65, 0.8, 1.1, 1.2, ...]], "y":[[2.31, 2.25, 2.0, 1.87, 1.66, ...]], "length":[[40.1]], "width":[[3.1]], "outline":[["11", "911", "118", "MMMeeMUGEEUUFFIIUVIVPc<<c<3<30`02<0228P0"]]},
+#     {"id":"worm1", "t":[0.3], "x":[[0.27, 0.6, 0.75, 1.0, 1.1, ...]], "y":[[2.4, 2.3, 2.07, 1.78, 1.75, ...]]}, "length":[[64.3]], "width":[[13.2]], "outline":[["1040", "2303", "237", "MMGGMGEGEEeEMMEWedPX82080020202280P"]]}
+#     ]
+# }
+
+# pass a config file (delimited, headered) listing per line:
+# (1) a listing of .RData files (Parsed output from Choreography)
+# (2) a prefix for the .wcon.zip (if blank/NA, the input file trimmed of prefix/suffix is used)
+# (3:N) metadata (with names taken from the header).
+# For CeMEE: 
+#   sample      RIL/population ID
+#   sample_type RIL, population, founder, MA etc
+#   who         experimenter
+#   place       Paris/Lisbon
+#   date        YYYYMMDD
+#   time        HHMMSS
+#   media       NGM/NaCl
+#######################################################################
+
+require(jsonlite)
+require(data.table)
+require(parallel)
+require(lubridate)
+
+args = commandArgs(trailingOnly = T)
+cfg  = args[1]  # config file
+outd = args[2]  # all output written to common dir 
+
+# unarray unit values recursively
+unb <- function(i){
+  if(length(i)==1){
+    unbox(i)
+  } else {
+    lapply(i, function(j) unb(j))
+  }
+}
+
+# common metadata tags
+metal <- list(lab="EEV")
+
+# common units list for metadata and data
+unitl = lapply(
+  list(t='s',
+       x='mm',
+       y='mm',
+       length='mm',
+       width='mm',
+       area='mm^2',
+       speed='mm/s',
+       curve='r',
+       angular='r/s',
+       bias='',
+       food='HT115',
+       software=list(name='MWT', 
+                     version='1.3.0_r1035',
+                     settings='-S --shadowless -q --plugin Reoutline --plugin Respine -N all'
+       )
+  ), 
+  function(i) unb(i)
+)
+
+extractRda <- function(rda){
+    
+  load(rda)
+  res = results[[3]]
+  names(res)[names(res)=='XPosition'] <- 'x'
+  names(res)[names(res)=='Yposition'] <- 'y'
+  res <- na.exclude(res[,c('Time', 'IndWorm', 'x', 'y', 'Velocity', 'Bias', 'Area', 'Length', 'Width', 'Curvature', 'TurningRate')])
+  
+  lapply(split(res, res$IndWorm), function(b) {
+    
+    list(id = unbox(b$IndWorm[1]),
+         t = list(b$Time),
+         x = list(b$x),
+         y = list(b$y),
+         "@MWT" = list(angular = list(b$TurningRate),
+                       area = list(b$Area),
+                       bias = list(b$Bias),
+                       curve = list(b$Curvature),
+                       length = list(b$Length),
+                       speed = list(b$Velocity),
+                       width = list(b$Width)
+         )
+    )
+    
+  })
+  
+}
+
+main <- function(cfg, outd, np=1){
+  
+  setwd(outd)
+  config = fread(cfg, data.table = F, colClasses = c(date='character', time='character'))
+  nda = nrow(config)
+  
+  config = config[order(config$sample_type, config$sample),]
+  rdas = config[,1]
+  prefs = config[,2]
+  metadata = config[,3:ncol(config)]
+  tstamp = ymd_hms(paste(metadata$date,metadata$time))
+  tstamp = sapply(1:nda, function(i) strftime(tstamp[i], '%Y-%m-%dT%H:%M:%SZ', tz = sprintf('Europe/%s', metadata$place[i])))
+  metadata$timestamp = tstamp
+  metadata = metadata[,!names(metadata) %in% c('date', 'time')]
+  
+  o = mclapply(1:nda, mc.cores = np, function(i) {
+    
+    rda = rdas[i]
+    pref = ifelse(is.na(prefs[i]), gsub('Parsed_data-', '', gsub('.RData', '', basename(rda))), prefs[i])
+    outf = sprintf('%s.wcon', pref)
+    osha = sprintf('%s.sha', outf)
+    ozip = sprintf('%s.zip', outf)
+    
+    if(!file.exists(ozip)){
+      cat(sprintf('converting %s\n', rda))
+      
+      oi = list(
+        metadata = unb(c(metal, metadata[i,])),
+        units = unitl,
+        data = extractRda(f)
+      )
+      
+      write_json(oi, outf, pretty=T)
+      system(sprintf('shasum %s > %s', outf, osha))
+      zip(ozip, files = c(outf, osha))
+      system(sprintf('rm %s %s', outf, osha))  
+    } else {
+      cat(sprintf('skipping %s\n', rda))
+    }
+    
+  })
+  
+}
+
+cfg = '~/rdas.cfg'
+outd = '~/MWTdata/wcon'
+main(cfg, outd, np=10)
+
+
+makeRdaCfg <- function(){
+  
+  # thor
+  # make cfg for all parsed data
+  
+  pdir = system(sprintf('find %s -name "Parsed*RData"', '/users/gev/mallard/Data/Cemee_data/'), intern=T)
+  pdir = c(pdir, system('find /users/gev/mallard/Data/Parsed_behavior/B[0-9]* -name "Parsed*.RData"', intern = T))
+  
+  rdas = data.frame(rda=pdir, stringsAsFactors = F)
+  bn = basename(rdas$rda)
+  sp = tstrsplit(bn, '-')
+  dd = gsub('_proc', '', sp[[3]])
+  rdas$date = tstrsplit(dd, '_')[[1]]
+  rdas$time = substr(tstrsplit(dd, '_')[[2]], 1, 6)
+  rdas$block = sp[[2]]
+  mm = gsub('.RData', '', sp[[4]])
+  spp = tstrsplit(mm, '_')
+  rdas$id = spp[[1]]
+  rdas$media = spp[[2]]
+  rdas = subset(rdas, media!='control')
+  rdas = subset(rdas, block!='MaleControlBlock')
+  rdas$media[rdas$media=='NGMl'] = 'NGM'
+  # experimenter
+  rdas$year = as.numeric(substr(rdas$date, 1, 4))
+  rdas <- subset(rdas, year!=2015)
+  rdas$who = 'BA'
+  rdas$who[year==2016] <- 'AC'
+  rdas$who[year %in% c(2018, 2019)] <- 'FM'
+  # place
+  rdas$place = 'Paris'
+  rdas$place[rdas$year == 2012] = 'Lisbon'
+  # sample/type
+  rdas$sample = gsub('L0', '', rdas$id)
+  rdas$sample_type = NA
+  rdas$sample[rdas$id=='N2anc' & !rdas$block %in% c('B302','B305')] = 'N2'
+  # reformat
+  x = "G50MOVA"; ix = grep(x, rdas$sample)
+  rdas$sample[ix] <- gsub(x, '', rdas$sample[ix])
+  rdas$sample[ix] <- sprintf('GA%s50%s', substr(rdas$sample[ix], 1, 1), substr(rdas$sample[ix], 2, 5))
+  x = "G140A6"; ix = grep(x, rdas$sample)
+  rdas$sample[ix] <- sprintf('A6140%s', gsub(x, '', rdas$sample[ix]))
+  
+  rdas$sample_type[grep('^PB250', rdas$sample)] <- 'MA line'
+  rdas$sample_type[grep('^N2250', rdas$sample)] <- 'MA line'
+  rdas$sample_type[rdas$sample=='PBanc'] = 'MA founder'
+  rdas$sample_type[rdas$sample=='N2'] = 'MA founder'
+  ix = grep('L', rdas$sample, invert = T); ix = ix[is.na(rdas$sample_type[ix])]
+  rdas$sample_type[ix] = 'population'
+  cemeeFounders <- sort(c('AB1', 'CB4507', 'CB4855', 'CB4856', 'CB4858', 'MY1', 'MY16', 'JU400', 'JU319', 'RC301', 'PX179', 'N2anc', 'PB306', 'PX174', 'CB4852', 'JU345'))
+  rdas$sample_type[rdas$sample %in% cemeeFounders] <- 'founder'
+  rdas$sample_type[is.na(rdas$sample_type)] = 'RIL'
+  # Patrick's lines
+  rdas$sample_type[rdas$sample == 'CX12311'] = 'NIL'
+  rdas$sample_type[rdas$sample == 'LSJ2'] = 'mutant'
+  # exc. male depleted samples
+  rdas = rdas[grep('noM', rdas$sample, invert = T),]
+  # Bruno's old N2
+  rdas <- subset(rdas, !(who=='BA' & sample=='N2'))
+  
+  rdas$pref=sprintf("%s_%s_%s", rdas$sample, rdas$date, rdas$time)
+  rdas = rdas[,c('rda','pref','sample','sample_type','who','place','date','time','media')]
+  rdas = rdas[order(as.numeric(rdas$date), as.numeric(rdas$time)),]
+  fwrite(rdas, '~/rdas.cfg')
+  
+  # names(bt)[names(bt) %in% metad$data_group_name]
+  # names(bt)[!names(bt) %in% metad$data_group_name]
+  # meh ignore metadata. get line/food for blobs filenames
+  # metad <- fread('~/Documents/cemee/MWT2WCON/Luke_header.txt')
+  # names(metad)[6:7] = c('date', 'time')
+  
+}
+
